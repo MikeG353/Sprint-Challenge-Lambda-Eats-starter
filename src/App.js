67699ec416b0b6788b255d2c2918d6c3deed4452
Michael Gregory <michael.gregory8584@gmail.com>
2020-04-24 12:52:01 -0500

    Still working on submit logic. definate bugs checkboxes don't behave properly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, Router, Link } from 'react-router-dom'
 import Form from './Form'
 
+const initialFormValues = {
+  customer_name:'',
+  size:'',
+  sauce:'',
+  toppings:'',
+  instructions:''
+}
+
 const App = () => {
+  const [orderList, setOrderList] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const onInputChange = evt => {
+    const name = evt.target.name
+    const value = evt.target.value
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const onCheckboxChange = evt => {
+    const {name} = evt.target
+    const isChecked =evt.target.isChecked
+    setFormValues({
+      ...formValues,
+      toppings: {
+        ...formValues.toppings,
+        [name]: isChecked
+      }
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    const newOrder = {
+      customer_name : formValues.customer_name,
+      size: formValues.size,
+      sauce: formValues.sauce,
+      toppings: Object.keys(formValues.toppings)
+        .filter(topping => formValues.toppings[topping] === true)
+    }
+    setOrderList([...orderList, newOrder])
+    setFormValues(initialFormValues)
+  }
+
   return (
     <div>
       {/* <Header > will put the below inside a header onject since it doesnt change in the wireframe*/}
@@ -16,7 +62,12 @@ const App = () => {
       {/* </Header> */}
       <Switch>
         <Route path='/Pizza'>
-          <Form></Form>
+          <Form
+          values={formValues}
+          onInputChange={onInputChange}
+          onCheckboxChange={onCheckboxChange}
+          onSubmit={onSubmit}
+          />
           
         </Route>
         <Route path='/'>
@@ -25,7 +76,7 @@ const App = () => {
             <div className={'hero-image container'}>
               <img src={'./image/Pizza.jpg'}/>
               <div className={"centered"}>Your favorite food, delivered while coding</div>
-              <button>Click!</button> {/* this button will lead to the form */}
+              <Link to='/Pizza'><button>Click!</button></Link> {/* this button will lead to the form */}
             </div>
         </Route>
       </Switch>
--- src/Form.js
+++ src/Form.js
@@ -15,10 +15,17 @@ export default function Form(props) {
             </div>
             <h2>Build Your Own Pizza</h2>
             <img src='./image/Pizza'></img>
+            <label>Name:&nbsp;
+                <input
+                    value={values.customer_name}
+                    onChange={onInputChange}
+                    name='customer_name'
+                    type='text'/>
+            </label>
             <label>Choice of Size:&nbsp;
                 <select 
                     value={values.size}
-                    onchange={onInputChange}
+                    onChange={onInputChange}
                     name='size'
 
                 >
@@ -28,14 +35,64 @@ export default function Form(props) {
                     <option value='Large'>Large</option>
                 </select>
             </label>
-            <label>Choice of Toppings
+            <label>Coice of Sauce:&nbsp;
+                <input
+                type='radio'
+                name='original'
+                value='original'
+                checked={true}
+                />Original Red
+                <input
+                type='radio'
+                name='garlic_ranch'
+                value='garlic_ranch'
+                />Garlic Ranch
+                <input
+                type='radio'
+                name='bbq'
+                value='bbq'
+                />BBQ Sauce
+                 <input
+                type='radio'
+                name='alfredo'
+                value='alfredo'
+                />Alfredo
+
+            </label>
+            <label>Choice of Toppings&nbsp;
                 <input
                 checked={values.pepperoni}
                 onChange={onCheckboxChange}
                 name='pepperoni'
-                type='checkbox'>Pepperonni</input>
+                type='checkbox'/>Pepperonni
+
+                <input
+                checked={values.sausage}
+                onChange={onCheckboxChange}
+                name='sausage'
+                type='checkbox'/>Sausage
+
+                <input
+                checked={values.canadian_bacon}
+                onChange={onCheckboxChange}
+                name='canadian_bacon'
+                type='checkbox'/>Canadian Bacon
+
+                <input
+                checked={values.bacon}
+                onChange={onCheckboxChange}
+                name='bacon'
+                type='checkbox'/>Bacon
 
             </label>
+            <label>Special Delivery Instructions
+                <input
+                    value={values.instructions}
+                    onChange={onInputChange}
+                    name='instructions'
+                    type='text'/>
+            </label>
+            <button onClick={onSubmit}>Place Order</button>
 
         </form>
     )
